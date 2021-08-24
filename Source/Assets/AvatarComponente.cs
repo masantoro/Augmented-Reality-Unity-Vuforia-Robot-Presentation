b4fd0b65d5a3f3f1a283999ce3e6ef5939095c74
Marco Antonio Santoro Soares <marco.santoro.soares@avanade.com>
2021-08-24 17:44:14 -0300

    adding project

---
+++ Source/Assets/AvatarComponente.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarComponente : MonoBehaviour {
+    bool gatilho;
+    bool pulando;
+    public Animator animatorController;
+
+    void Awake()
+    {
+        if (!animatorController)
+            animatorController = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            pulando = !pulando;
+            animatorController.SetBool("Iniciar", pulando);
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            gatilho = !gatilho;
+            animatorController.SetBool("Gatilho", gatilho);
+        }
+	}
+
+
+
+}
---
+++ Source/Assets/PlayTarget_BH.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+public class PlayTarget_BH : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+    public Animator animatorController;
+    public Avatar avatarAnimation;
+    public GameObject Cylinder;
+
+    void Awake()
+    {
+        if (!animatorController)
+            animatorController = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+
+        Cylinder = GameObject.Find("Cylinder");
+
+    }
+
+    void Update()
+    {
+        if (!GetComponent<AudioSource>().isPlaying)
+        {
+            animatorController.SetBool("Iniciar", false);
+            animatorController.Play("Starting");
+            //animatorController.Rebind();
+        }
+
+        //Cylinder.transform.Rotate(new Vector3(0,Time.deltaTime * 50,0));
+
+        //transform.Rotate(new Vector3(0, Time.deltaTime + 50, 0));
+
+    }
+
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+
+            if (!animatorController.GetBool("Iniciar"))
+            {
+                animatorController.transform.localPosition = new Vector3(0.002f, 1.349f, -0.499f);
+                animatorController.SetBool("Iniciar", true);
+                GetComponent<AudioSource>().Play();
+            }
+        }
+        else
+        {
+            // Stop audio when target is lost
+            //animatorController.SetBool("Iniciar", false);
+        }
+    }
+}
